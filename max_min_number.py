a9427f73944e14c458007ad0f6f3f6380c7ecf18
Sabeen13 <basnets131@gmail.com>
2024-08-28 23:26:16 -0500

    Finding the max and min value from the list

---
+++ max_min_number.py
@@ -0,0 +1,7 @@
+#Finding the max and min value from the list
+list = [1,2,3,4,5,6,20,100,50,200,500]
+
+max_num = max(list)
+min_num = min(list)
+
+print(f'The maximum value is:{max_num}' ' and' f' minimum value is:{min_num}')
